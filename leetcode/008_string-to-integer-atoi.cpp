07e2d9e8ba9e5e3574824555356b58154e08e836
hdqh88 <hao.ding@a8c.com>
2022-07-18 17:11:03 -0400

    008_string-to-integer-atoi.cpp

--- leetcode/008_string-to-integer-atoi.cpp
+++ leetcode/008_string-to-integer-atoi.cpp
@@ -24,10 +24,49 @@ using namespace std;
 
 class Solution_008 {
 public:
-    int myAtoi(string str) {
-        int top = INT_MAX / 10, bottom = INT_MIN / 10;
-        int top_remainder = INT_MAX % 10, bottom_remainder = INT_MIN % 10;
-        int result = 1;
+    int myAtoi(string s) {
+        int top = INT_MAX / 10;
+        int top_remainder = INT_MAX % 10;
+        int len = s.size();
+        int result = 0;
+
+        int i = 0;
+        // Discard all spaces from the beginning of the input string.
+        while(s[i] == ' ' && i < len)
+            i++;
+        if(i < len){
+            // sign = +1, if it's positive number, otherwise sign = -1.
+            int sign = 1;
+            if(s[i] == '+')
+                i++;
+            else if(s[i] == '-'){
+                sign = -1;
+                i++;
+            }
+            for(;i < len; i++){
+                // Traverse next digits of input and stop if it is not a digit.
+                // End of string is also non-digit character.
+                if(s[i] < '0' || s[i] > '9')
+                    break;
+                // Check overflow and underflow conditions.
+                if(result > top || (result == top && s[i] - '0' > top_remainder))
+                    // If integer overflowed return 2^31-1, otherwise if underflowed return -2^31.
+                    return sign == 1 ? INT_MAX : INT_MIN;
+
+                // Append current digit to the result.
+                result = result * 10 + (s[i] - '0');
+            }
+            // We have formed a valid number without any overflow/underflow.
+            // Return it after multiplying it with its sign.
+            result *= sign;
+        }
+        return result;
 
     }
 };
+
+//int main() {
+//    string s = "21474836460";
+//    Solution_008 sol{};
+//    cout << sol.myAtoi(s) << endl;
+//}
--- prototype/callable-objects.cpp
+++ prototype/callable-objects.cpp
@@ -46,40 +46,40 @@ void print(vector<string>& vec) {
     cout << endl;
 }
 
-int main() {
-    vector<string> sentence{"which", "provide", "a", "way", "to", "pass", "around", "instructions", "on", "how", "to", "perform", "an", "operation"};
-
-    vector<string> s1 = sentence;
-    cout << "Sentence:" << endl;
-    print(s1);
-    /** Object of a function-obj class */
-    sort(s1.begin(), s1.end(), ClassShorter());
-    cout << "After sort with ClassShorter:" << endl;
-    print(s1);
-
-    vector<string> s2 = sentence;
-    cout << endl << "Sentence:" << endl;
-    print(s2);
-    /** Function converted to a function pointer */
-    sort(s2.begin(), s2.end(), isShorter);
-    cout << "After sort with isShorter:" << endl;
-    print(s2);
-
-    vector<string> s3 = sentence;
-    cout << endl << "Sentence:" << endl;
-    print(s3);
-    /** Lambda expr. */
-    sort(s3.begin(), s3.end(), lambdaShorter);
-    cout << "After sort with isShorter:" << endl;
-    print(s3);
-
-    pointerShorter = isShorter;
-    vector<string> s4 = sentence;
-    cout << endl << "Sentence:" << endl;
-    print(s4);
-    /** Function pointer */
-    sort(s4.begin(), s4.end(), pointerShorter);
-    cout << "After sort with isShorter:" << endl;
-    print(s4);
-
-}
+//int main() {
+//    vector<string> sentence{"which", "provide", "a", "way", "to", "pass", "around", "instructions", "on", "how", "to", "perform", "an", "operation"};
+//
+//    vector<string> s1 = sentence;
+//    cout << "Sentence:" << endl;
+//    print(s1);
+//    /** Object of a function-obj class */
+//    sort(s1.begin(), s1.end(), ClassShorter());
+//    cout << "After sort with ClassShorter:" << endl;
+//    print(s1);
+//
+//    vector<string> s2 = sentence;
+//    cout << endl << "Sentence:" << endl;
+//    print(s2);
+//    /** Function converted to a function pointer */
+//    sort(s2.begin(), s2.end(), isShorter);
+//    cout << "After sort with isShorter:" << endl;
+//    print(s2);
+//
+//    vector<string> s3 = sentence;
+//    cout << endl << "Sentence:" << endl;
+//    print(s3);
+//    /** Lambda expr. */
+//    sort(s3.begin(), s3.end(), lambdaShorter);
+//    cout << "After sort with isShorter:" << endl;
+//    print(s3);
+//
+//    pointerShorter = isShorter;
+//    vector<string> s4 = sentence;
+//    cout << endl << "Sentence:" << endl;
+//    print(s4);
+//    /** Function pointer */
+//    sort(s4.begin(), s4.end(), pointerShorter);
+//    cout << "After sort with isShorter:" << endl;
+//    print(s4);
+//
+//}
