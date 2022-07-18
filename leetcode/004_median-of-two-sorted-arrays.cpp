8f45287cdffb2d3a238b93c687bae51c9c9906e2
hdqh88 <hao.ding@a8c.com>
2022-07-18 01:28:18 -0400

    median-of-two-sorted-arrays

--- leetcode/004_median-of-two-sorted-arrays.cpp
+++ leetcode/004_median-of-two-sorted-arrays.cpp
@@ -20,13 +20,54 @@ using namespace std;
 
 class Solution_004 {
 public:
+    // BEST
+    // Exp1: https://www.youtube.com/watch?v=LPFhl65R7ww
+    // Exp2: https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2471/Very-concise-O(log(min(MN)))-iterative-solution-with-detailed-explanation
+    // Binary Search
+    // Time:    O(log(min(m, n)))
+    // Space:   O(1)
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        if (nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
+        int m = nums1.size(), n = nums2.size();
+
+        // Binary Search on the shorter array
+        int left = 0, right = m, half = (m + n + 1) / 2;
+        while (left <= right) {
+            int mid1 = (left + right) / 2;
+            int mid2 = half - mid1;
+
+            // if mid1 is 0, it means nothing is there on left side. Use INT_MIN for maxLeft1
+            // if mid1 is length of input, it means nothing is on right side. Use INT_MAX for minRight1
+            int maxLeft1 = (mid1 == 0) ? INT_MIN : nums1[mid1 - 1];
+            int minRight1 = (mid1 == m) ? INT_MAX : nums1[mid1];
+
+            int maxLeft2 = (mid2 == 0) ? INT_MIN : nums2[mid2 - 1];
+            int minRight2 = (mid2 == n) ? INT_MAX : nums2[mid2];
+
+            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+                // We have partitioned arrays at correct place
+                // Now get max of left elements and min of right elements to get the median in case of even length combined array size
+                // or get max of left for odd length combined array size
+                if ((m + n) % 2 == 0)
+                    return (max(maxLeft1, maxLeft2) * 1.0 + min(minRight1, minRight2)) / 2;
+                else
+                    return max(maxLeft1, maxLeft2);
+            } else if (maxLeft1 > minRight2)
+                right = mid1 - 1;
+            else
+                left = mid1 + 1;
+        }
+        return 0;
+    }
+
+
     // Exp: https://discuss.leetcode.com/topic/4996/share-my-o-log-min-m-n-solution-with-explanation
     // Time:	O(log(m+n))
     // Space:	O(1)
-    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+    double findMedianSortedArrays1(vector<int> &nums1, vector<int> &nums2) {
         int m = nums1.size(), n = nums2.size();
         if (m > n)
-            return findMedianSortedArrays(nums2, nums1);
+            return findMedianSortedArrays1(nums2, nums1);
         int i, j, imin = 0, imax = m, half = (m + n + 1) / 2;
         while (imin <= imax) {
             i = (imin + imax) / 2;
@@ -50,6 +91,7 @@ public:
         else num2 = min(nums1[i], nums2[j]);
         return (num1 + num2) / 2.0;
     }
+
 };
 
 
