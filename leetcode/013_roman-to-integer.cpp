7361151f5ed85dc9ee8c6909e642f20681ccdf94
hdqh88 <hao.ding@a8c.com>
2022-07-24 15:47:00 -0400

    013_roman-to-integer.cpp

--- leetcode/013_roman-to-integer.cpp
+++ leetcode/013_roman-to-integer.cpp
@@ -3,42 +3,36 @@
 //Input is guaranteed to be within the range from 1 to 3999.
 
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution_013 {
 public:
     // My own
-    // Time:	O(log(n))
+    // Time:	O(1)
     // Space:	O(1)
     int romanToInt(string s) {
-        int result;
-        for (int i = 0; i < s.size(); i++) {
-            int val1 = transfer(s[i]);
-            if (i < s.size() - 1) {
-                int val2 = transfer(s[i+1]);
-                if (val1 >= val2)
-                    result += val1;
-                else {
-                    result += val2 - val1;
-                    i++;
-                }
-            } else
-                result += val1;
+        unordered_map<char, int> values{
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
+        };
+        int result = 0, i = 0;
+        while (i < s.size()) {
+            if (i < s.size() - 1 && values[s[i]] < values[s[i + 1]]) {
+                result += values[s[i + 1]] - values[s[i]];
+                i += 2;
+            } else {
+                result += values[s[i]];
+                i++;
+            }
         }
         return result;
-    }
-
-    int transfer(char c) {
-        switch (c) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-        }
     }
 };
 
