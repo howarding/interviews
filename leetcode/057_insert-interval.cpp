1487a81b415c53d12e610eb78a5374957f138351
haoding <hdqh88@gmail.com>
2023-11-16 23:59:30 -0500

    057_insert-interval.cpp

--- leetcode/057_insert-interval.cpp
+++ leetcode/057_insert-interval.cpp
@@ -1,14 +1,33 @@
-//Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+//You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start
+// and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval
+// newInterval = [start, end] that represents the start and end of another interval.
 //
-//You may assume that the intervals were initially sorted according to their start times.
+//Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still
+// does not have any overlapping intervals (merge overlapping intervals if necessary).
+//
+//Return intervals after the insertion.
+//
+//
 //
 //Example 1:
-//Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
 //
+//Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+//Output: [[1,5],[6,9]]
 //Example 2:
-//Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
 //
-//This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+//Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+//Output: [[1,2],[3,10],[12,16]]
+//Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+//
+//
+//Constraints:
+//
+//0 <= intervals.length <= 104
+//intervals[i].length == 2
+//0 <= starti <= endi <= 105
+//intervals is sorted by starti in ascending order.
+//newInterval.length == 2
+//0 <= start <= end <= 105
 
 #include <iostream>
 #include <vector>
@@ -38,18 +57,19 @@ public:
     // Exp: https://discuss.leetcode.com/topic/12899/very-short-and-easy-to-understand-c-solution/4
     // Time:	O(n)
     // Space:	O(n)
-    vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
-        vector<Interval> result;
-        int n = intervals.size();
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> result;
+        const vector<vector<int>>::size_type n = intervals.size();
         int i = 0;
-        while (i < n && intervals[i].end < newInterval.start)
+        while (i < n && intervals[i].back() < newInterval.front()) {
             result.push_back(intervals[i++]);
-        newInterval.start = (i == n ? newInterval.start : min(newInterval.start, intervals[i].start));
-        while (i < n && intervals[i].start < newInterval.end)
+        }
+        newInterval.front() = i == n ? newInterval.front() : min(newInterval.front(), intervals[i].front());
+        while (i < n && intervals[i].front() <= newInterval.back())
             i++;
-        newInterval.end = (i == 0 ? newInterval.end : max(newInterval.end, intervals[i - 1].end));
+        newInterval.back() = i == 0 ? newInterval.back() : max(newInterval.back(), intervals[i - 1].back());
         result.push_back(newInterval);
-        while (i < n && newInterval.end < intervals[i].start)
+        while (i < n)
             result.push_back(intervals[i++]);
         return result;
     }
